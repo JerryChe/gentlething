5514c99cb8d8bb581b79ec0032d9dfd6ebdbc4b8
Mason <masonbeater@gmail.com>
2014-05-28 15:37:37 +0800

    project config
    加入spring和Hibernate特性，以及进行日志和事务的配置

---
+++ gentlething-web/src/main/java/com/gentlething/controller/IndexController.java
@@ -0,0 +1,17 @@
+/**
+ * File name : com.gentlething.controller.IndexController.java
+ * Create time : 2014年5月28日 下午12:05:56
+ * Author : sh03259 Mason
+ */
+package com.gentlething.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Mason
+ *
+ */
+@Controller("indexController")
+public class IndexController {
+
+}
